c6338b4fa9b69d60d8db2a22cc3b84a9d8eca5cd
Rcmaniac25 <rcmaniac25@hotmail.com>
2017-01-11 23:47:51 -0500

    Initial working version (no arguments besides an optional path)

--- bin/main.dart
+++ bin/main.dart
@@ -2,9 +2,14 @@ import 'dart:io';
 
 import 'package:args/args.dart';
 
+// Initial structure at this point based off https://www.dartlang.org/tutorials/dart-vm/cmdline
+// While 'ls' was chosen prior to looking at docs, a "dumb" version is defined https://www.dartlang.org/guides/libraries/library-tour#dartio---io-for-command-line-apps (Listing files in a directory)
+
 void main(List<String> args) {
   exitCode = 0;
   final parser = new ArgParser();
+
+  //TODO: figure out how to get console width (and what happens when the console is resized)
 
   var parsedArgs = parser.parse(args);
   var files = parsedArgs.rest;
@@ -13,13 +18,34 @@ void main(List<String> args) {
   if (files.isEmpty) {
   	dir = Directory.current;
   } else {
-  	dir = new Directory(files[0]); // XXX Does not appear to be handling relative paths as documented...
+  	dir = new Directory(files[0]);
   }
 
-  processArguments(dir, parsedArgs);
+  processArgumentsAndRun(dir, parsedArgs);
 }
 
-Future processArguments(Directory dir, ArgResults parsedArgs) {
-  // TODO
-  print('Dir ${dir.path}');
+Future processArgumentsAndRun(Directory dir, ArgResults parsedArgs) async {
+  if (await dir.exists()) {
+  	var valuesWritten = false;
+
+  	var dirEntities = dir.list();
+  	await for (FileSystemEntity entity in dirEntities) {
+  	  var entitySegments = entity.uri.pathSegments;
+  	  
+  	  var entityName = entitySegments.last;
+  	  if (entityName.isEmpty) {
+  	  	entityName = entitySegments[entitySegments.length - 2]; // Get the second-to-last
+	  }
+
+  	  if (entityName.isNotEmpty) {
+  	  	if (entityName[0] != '.') {
+  	  	  stdout.write("${entityName}${valuesWritten ? ' ' : ''}");
+  	  	  valuesWritten = true;
+  	  	}
+  	  }
+  	}
+  	stdout.writeln();
+  } else {
+  	stderr.writeln('ls: cannot access ${dir.path}: No such file or directory');
+  }
 }
